b9b4cde6087c6210d442a59d4e3b753d65064e05
Manav Desai <58055256+manavdesai17@users.noreply.github.com>
2021-02-04 17:40:33 -0500

    Made some changes

--- task2.c
+++ task2.c
@@ -3,26 +3,37 @@
 #include <unistd.h>
 
 int main(){
-    int pid1, pid2, pid3;
+    pid_t pid1, pid2, pid3;
 
     pid1 = fork();
-
-    if (pid1 == 0){ //executed the first child process
+    if (pid1 < 0){
         
+        fprintf(stderr, "fork failed.");
+		return 1;
+    }
+    else if (pid1 == 0){ //executed the first child process
         printf("First child is executed. PID = %d.\n", getpid());  
+        
     } else {
         pid2 = fork();
-        if (pid2 == 0){ //executed the second child process
-           
+        if (pid2 < 0){ 
+            fprintf(stderr, "fork failed.");
+            return 1;
+        }
+        else if (pid2 == 0){ //executed the second child process
             printf("Second child is executed. PID = %d\n", getpid());
+            
         } else {
             pid3 = fork();
-            if (pid3 == 0){ //executed the third child process
+            if (pid3 < 0){
+                fprintf(stderr, "fork failed.");
+                return 1;
+            }
+            else if (pid3 == 0){ //executed the third child process
+                printf("Third child is executed. PID = %d\n", getpid());
                 
-                printf("third child is executed. PID = %d\n", getpid());
             }
             else { //executed the parent process
-                
                 printf("Parent process is executed. PID = %d\n", getpid());
             }
         }
